9201f76cafbf0d4464e74d36e3f699de0eae9443
Garrett <gvk795@gmail.com>
2020-07-26 03:16:10 -0700

    all images links being printed

--- scrape.py
+++ scrape.py
@@ -5,5 +5,13 @@ with open('simple.html') as html_file: # open html file
     soup = BeautifulSoup(html_file, 'lxml') # soup = parsed html
 
 # print product names
-for product_name in soup.find_all('font', style = 'display: block;height:40px;text-transform: uppercase;'):
-    print(product_name.text)
+# for product_name in soup.find_all('font', style = 'display: block;height:40px;text-transform: uppercase;'):
+#     print(product_name.text)
+
+# finds categories but doesn't find fashion
+# for product_name in soup.find_all('a', style = 'font-family:eurof;font-size:14px;'):
+#     print(product_name.text)
+
+# img#main_img
+for product_name in soup.find_all('img'):
+    print(product_name['src'])
