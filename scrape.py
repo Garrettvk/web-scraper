5ef8541f66cc08f469d2029f2fdd7cbac465e623
Garrett <gvk795@gmail.com>
2020-07-26 02:45:22 -0700

    comments added to scrap.py

--- scrape.py
+++ scrape.py
@@ -1,10 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.wonatrading.com/jewelry/anklet'
+with open('simple.html') as html_file: # open html file
+    soup = BeautifulSoup(html_file, 'lxml') # soup = parsed html
 
-with open('simple.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
-
+# print product names
 for product_name in soup.find_all('font', style = 'display: block;height:40px;text-transform: uppercase;'):
     print(product_name.text)
