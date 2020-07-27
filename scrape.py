5229fd2ae62f55b4e23ebe3fda0ae9ece917bfe6
Garrett <gvk795@gmail.com>
2020-07-26 20:21:19 -0700

    image links isolated

--- scrape.py
+++ scrape.py
@@ -4,14 +4,23 @@ import requests
 with open('simple.html') as html_file: # open html file
     soup = BeautifulSoup(html_file, 'lxml') # soup = parsed html
 
+product_names = []
+
 # print product names
-# for product_name in soup.find_all('font', style = 'display: block;height:40px;text-transform: uppercase;'):
-#     print(product_name.text)
+for product_name in soup.find_all('font', style = 'display: block;height:40px;text-transform: uppercase;'):
+    product_names.append(product_name.text)
 
 # finds categories but doesn't find fashion
+# fashion is found when you click on the item
 # for product_name in soup.find_all('a', style = 'font-family:eurof;font-size:14px;'):
 #     print(product_name.text)
 
-# img#main_img
-for product_name in soup.find_all('img'):
-    print(product_name['src'])
+# prints image links
+for title in product_names:
+
+    for product_name in soup.find_all('img', title = title):
+
+        domain = 'https://www.wonatrading.com/'
+        image_link = product_name['src']
+        output = domain + image_link
+        print(output)
