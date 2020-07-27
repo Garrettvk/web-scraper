aa394786a964271d9c2638652835b03a5f132046
Garrett <gvk795@gmail.com>
2020-07-26 23:40:50 -0700

    one product sucsessfully webscrapped

---
+++ scrape_one_product.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from clean_description import *
+
+with open('simple.html') as html_file:  # open html file
+    soup = BeautifulSoup(html_file, 'lxml')  # soup = parsed html
+
+product_name = soup.find('h2').text
+
+categories = soup.find_all('a', style='font-family:eurof;font-size:14px;')
+
+category1, category2, category3 = categories[0].text, categories[1].text, categories[2].text
+
+image1 = soup.find('img', id='main_img')['src']
+
+image2 = soup.find('img', id='des_img')['src']
+
+# raw description
+description = soup.find('td', class_='tdBorder').find('p').text
+
+# clean description
+description = clean_description(description)
+
+# raw price
+price = soup.find('td', class_='products_info_price').text
+
+# cleaned price
+price = clean_price(price)
+
+# each product attribute is put into a list
+data = [product_name, category1, category2,
+        category3, image1, image2, description, price]
+
+# column name is from webscrapper output
+columns = ['productname', 'category1', 'category2',
+           'category3', 'image1', 'image2', 'description', 'price']
+
+# create dataframe, notice that data list is wrapped in brackets
+df = pd.DataFrame(columns = columns, data = [data])
+
+# write dataframe to csv
+df.to_csv('output.csv', index = False)
