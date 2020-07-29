ec535035744f77c860255effaef05fa79c1ad6a6
Garrett <gvk795@gmail.com>
2020-07-29 12:08:25 -0700

    project organized

--- python/functions.py
+++ python/functions.py
@@ -1,12 +1,56 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-from clean_description import *
-from get_product_links import get_product_links
 import time
 
+def clean_description(input_string):
 
-def scrape_one_product(product_page_url):
+    remove_list = ['�', '\n', '\t', '•'] # items to remove
+
+    for element in remove_list: # iterate over items in list
+        input_string = input_string.replace(element, '') # remove each item
+
+    return input_string.strip() # return cleaned input string
+
+def clean_price(input_string):
+    # $4.25 / pc
+
+    remove_list = ['$', '/ pc', '\n',] # items to remove
+
+    for element in remove_list: # iterate over items in list
+        input_string = input_string.replace(element, '') # remove each item
+
+    return input_string.strip()
+
+def get_product_links():
+
+    product_links = []  # this is product urls for all the products on the first page
+
+    products_url = 'https://www.wonatrading.com/jewelry/anklet'  # url for login page
+    # second_page = 'https://www.wonatrading.com/jewelry/anklet/page=2'
+    domain = 'https://www.wonatrading.com/' # domain of website
+
+    headers = {
+        'cookie' : 'cookie_test=please_accept_for_session; __auc=8afe36de17389481e509d9e1c71; _ga=GA1.2.1781461277.1595736073; __tawkuuid=e::wonatrading.com::fl9A0cET/otx3JZxKB32n8j9xbPsMZQihfmL3WpUIh9u8t5UPxjwBuOSWYmVuhza::2; __adroll_fpc=76b3cd1ee25f0f80997d6c6b0f53fd86-1595736074261; _fbp=fb.1.1595736074767.210364020; _gid=GA1.2.1153286955.1595996477; osCsid=o9t4vidjg18hblbpoj8h93chc1; __asc=4c7656061739a9974840637ce99; _gat_gtag_UA_46005179_1=1; TawkConnectionTime=0; __ar_v4=G3NB2AWGMJALLM3LQB7B6W%3A20200725%3A66%7CYCTJCPNBHJAIPIVE526GC4%3A20200725%3A66%7CLM3XUYFU3ZECHJECYX2EJI%3A20200725%3A66',
+        'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
+    }
+
+    with requests.Session() as session:  # open html file    
+        
+        request = session.get(products_url, headers=headers).content # request products page
+        soup = BeautifulSoup(request, 'html5lib')  # soup = parsed html
+
+        # this html element contains the href for the product
+        products = soup.find_all('a', style="position:relative;float:left;")
+
+        for product in products:  # iterate each product on webpage
+            product_link = product['href']  # assign href to a variable
+            product_link = product_link.replace('\r\n', '')  # remove hidden characters from href
+            product_links.append(f'{domain}{product_link}')  # add domain
+            
+    return product_links
+
+def scrape_data(product_page_url): # gets data from single page
 
     headers = {
         'cookie': 'cookie_test=please_accept_for_session; __auc=8afe36de17389481e509d9e1c71; _ga=GA1.2.1781461277.1595736073; __tawkuuid=e::wonatrading.com::fl9A0cET/otx3JZxKB32n8j9xbPsMZQihfmL3WpUIh9u8t5UPxjwBuOSWYmVuhza::2; __adroll_fpc=76b3cd1ee25f0f80997d6c6b0f53fd86-1595736074261; _fbp=fb.1.1595736074767.210364020; osCsid=lcau16rhuad4cgo3nqt1g6k103; __asc=e88cdaa517398cd92b44cd05553; _gid=GA1.2.1153286955.1595996477; _gat_gtag_UA_46005179_1=1; TawkConnectionTime=0; __ar_v4=G3NB2AWGMJALLM3LQB7B6W%3A20200725%3A52%7CYCTJCPNBHJAIPIVE526GC4%3A20200725%3A52%7CLM3XUYFU3ZECHJECYX2EJI%3A20200725%3A52',
@@ -69,8 +113,7 @@ def scrape_one_product(product_page_url):
 
         return data
 
-
-def scrape_data(pages):
+def get_data(pages): # iterate over pages and create dataframe
 
     data = []  # list of data from each page
 
@@ -81,7 +124,7 @@ def scrape_data(pages):
     try: # try while ip isn't blocked
         for page in pages:  # iterate over html for each product
             # time.sleep(60) # sleep function limits the number of requests over time
-            page = scrape_one_product(page) # page = return of scrape_one_product function
+            page = scrape_data(page) # page = return of scrape_data function
             data.append(page)  # add data from page
 
     except AttributeError:      
--- python/main.py
+++ python/main.py
@@ -1,17 +1,10 @@
 import pandas as pd
-from scrape_one_product import *
+from functions import *
 
-# prints entire column width
-pd.set_option('display.max_colwidth', 1000)
-
-# show all columns and rows
-pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None) # prints entire column width
+pd.set_option('display.max_columns', None) # show all columns and rows
 pd.set_option('display.max_rows', None)
 
 pages = get_product_links()[0:2] # first 2 pages
-
-df = scrape_data(pages) # dataframe from scrape_data function
-
-df.to_csv('output.csv', index = False) # write dataframe to csv
-
-print(df) # print df
+df = get_data(pages) # dataframe from get_data function
+df
