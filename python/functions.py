5778f5ccf9b4bc7193b61f8cf55997982a8cc11c
Garrett <gvk795@gmail.com>
2020-07-31 02:44:17 -0700

    program scrapes 1 product

--- python/functions.py
+++ python/functions.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
 import pandas as pd
 import time
+
+# path to chromedriver
+driver = webdriver.Chrome(r'C:\Users\admin\Anaconda3\Lib\site-packages\chromedriver\chromedriver.exe')
 
 def clean_description(input_string):
 
@@ -26,99 +29,83 @@ def get_product_links():
 
     product_links = []  # this is product urls for all the products on the first page
 
-    products_url = 'https://www.wonatrading.com/jewelry/anklet'  # url for login page
-    # second_page = 'https://www.wonatrading.com/jewelry/anklet/page=2'
-    domain = 'https://www.wonatrading.com/' # domain of website
+    domain = r'https://www.wonatrading.com/'
+    product_page = r'https://www.wonatrading.com/jewelry/anklet'
 
-    headers = {
-        'cookie' : 'cookie_test=please_accept_for_session; __auc=8afe36de17389481e509d9e1c71; _ga=GA1.2.1781461277.1595736073; __tawkuuid=e::wonatrading.com::fl9A0cET/otx3JZxKB32n8j9xbPsMZQihfmL3WpUIh9u8t5UPxjwBuOSWYmVuhza::2; __adroll_fpc=76b3cd1ee25f0f80997d6c6b0f53fd86-1595736074261; _fbp=fb.1.1595736074767.210364020; _gid=GA1.2.1153286955.1595996477; osCsid=o9t4vidjg18hblbpoj8h93chc1; __asc=4c7656061739a9974840637ce99; _gat_gtag_UA_46005179_1=1; TawkConnectionTime=0; __ar_v4=G3NB2AWGMJALLM3LQB7B6W%3A20200725%3A66%7CYCTJCPNBHJAIPIVE526GC4%3A20200725%3A66%7CLM3XUYFU3ZECHJECYX2EJI%3A20200725%3A66',
-        'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
-    }
+    driver.get(domain)  # load hompage first so that it picks up cookies
+    driver.get(product_page)  # first product page
 
-    with requests.Session() as session:  # open html file    
-        
-        request = session.get(products_url, headers=headers).content # request products page
-        soup = BeautifulSoup(request, 'html5lib')  # soup = parsed html
+    soup = BeautifulSoup(driver.page_source, 'html5lib')  # soup = parsed html
 
-        # this html element contains the href for the product
-        products = soup.find_all('a', style="position:relative;float:left;")
+    # this html element contains the href for the product
+    products = soup.find_all('a', style="position:relative;float:left;")
 
-        for product in products:  # iterate each product on webpage
-            product_link = product['href']  # assign href to a variable
-            product_link = product_link.replace('\r\n', '')  # remove hidden characters from href
-            product_links.append(f'{domain}{product_link}')  # add domain
+    for product in products:  # iterate each product on webpage
+        product_link = product['href']  # assign href to a variable
+        product_link = product_link.replace('\r\n', '')  # remove hidden characters from href
+        product_links.append(f'{domain}{product_link}')  # add domain
             
     return product_links
 
-def scrape_data(product_page_url): # gets data from single page
+def scrape_data(product_page_url):  # gets data from single page
 
-    headers = {
-        # cookie from pressing login
-        'cookie': 'cookie_test=please_accept_for_session; __auc=8afe36de17389481e509d9e1c71; _ga=GA1.2.1781461277.1595736073; __tawkuuid=e::wonatrading.com::fl9A0cET/otx3JZxKB32n8j9xbPsMZQihfmL3WpUIh9u8t5UPxjwBuOSWYmVuhza::2; __adroll_fpc=76b3cd1ee25f0f80997d6c6b0f53fd86-1595736074261; _fbp=fb.1.1595736074767.210364020; _gid=GA1.2.1153286955.1595996477; osCsid=5hd6krf8r4fjpo38k8fupefdo7; __asc=45f23bee173a221c9ecf1ac3f9a; TawkConnectionTime=0; __ar_v4=LM3XUYFU3ZECHJECYX2EJI%3A20200725%3A101%7CYCTJCPNBHJAIPIVE526GC4%3A20200725%3A101%7CG3NB2AWGMJALLM3LQB7B6W%3A20200725%3A101',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
-    }
+    # login
+    # turn this off in production, use method up top ^
+    username = 'tmebatson@gmail.com'
+    password = 'hRIcV'
 
-    product_headers = {
-        # cookie from product page
-        'cookie': 'cookie_test=please_accept_for_session; __auc=8afe36de17389481e509d9e1c71; _ga=GA1.2.1781461277.1595736073; __tawkuuid=e::wonatrading.com::fl9A0cET/otx3JZxKB32n8j9xbPsMZQihfmL3WpUIh9u8t5UPxjwBuOSWYmVuhza::2; __adroll_fpc=76b3cd1ee25f0f80997d6c6b0f53fd86-1595736074261; _fbp=fb.1.1595736074767.210364020; _gid=GA1.2.1153286955.1595996477; __asc=45f23bee173a221c9ecf1ac3f9a; osCsid=i2ur198vro7qetrco32fs1i9e0; _gat_gtag_UA_46005179_1=1; TawkConnectionTime=0; __ar_v4=G3NB2AWGMJALLM3LQB7B6W%3A20200725%3A121%7CYCTJCPNBHJAIPIVE526GC4%3A20200725%3A121%7CLM3XUYFU3ZECHJECYX2EJI%3A20200725%3A121',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
-    }
+    # load hompage first so that it picks up cookies
+    driver.get(r'https://www.wonatrading.com/')
+    driver.get(r'https://www.wonatrading.com/login')  # wona login page
 
-    login_data = {
-        'action': 'process',
-        'email_address': 'tmebatson@gmail.com',
-        'password': 'hRIcV',
-        'x': '61',
-        'y': '8',
-    }
+    username_textbox = driver.find_element_by_name(
+        'email_address')  # find element for email input
+    username_textbox.send_keys(username)  # send what you put as userinput
 
-    with requests.Session() as session:  # open session
+    password_textbox = driver.find_element_by_name(
+        'password')  # find element for password input
+    password_textbox.send_keys(password)  # send what you put as password
 
-        # login
-        login_url = 'https://www.wonatrading.com/login'  # url for login page
-        request = session.get(login_url, headers=headers)  # request login page
-        request = session.post(login_url, data=login_data,
-                               headers=headers)  # send data to login
+    login_button = driver.find_element_by_id(
+        'btnLogin')  # element of login button
+    login_button.click()  # clicks login button
 
-        # request webpage of product
-        request = session.get(
-            product_page_url, headers=product_headers).content
+    driver.get(product_page_url)  # open first product page
 
-        soup = BeautifulSoup(request, 'lxml')  # soup = parsed html
+    soup = BeautifulSoup(driver.page_source, 'lxml')  # soup = parsed html
 
-        product_name = soup.find('h2').text
+    product_name = soup.find('h2').text
 
-        categories = soup.find_all(
-            'a', style='font-family:eurof;font-size:14px;')
+    categories = soup.find_all(
+        'a', style='font-family:eurof;font-size:14px;')
 
-        # combine all 3 categories and seperate with ; and /
-        Category = f'{categories[0].text};{categories[1].text}/{categories[2].text}'
+    # combine all 3 categories and seperate with ; and /
+    Category = f'{categories[0].text};{categories[1].text}/{categories[2].text}'
 
-        image1 = soup.find('img', id='main_img')['src']
+    image1 = soup.find('img', id='main_img')['src']
 
-        try: # try looking for second image
-            image2 = soup.find('img', id='des_img')['src']
+    try:  # try looking for second image
+        image2 = soup.find('img', id='des_img')['src']
 
+    except TypeError:  # if there's no image it will raise a type error and stop the program
+        image2 = ''  # all we need is an empty string
 
-        except TypeError: # if there's no image it will raise a type error and stop the program
-            image2 = '' # all we need is an empty string
+    # raw description
+    description = soup.find('td', class_='tdBorder').find('p').text
 
-        # raw description
-        description = soup.find('td', class_='tdBorder').find('p').text
+    # clean description
+    description = clean_description(description)
 
-        # clean description
-        description = clean_description(description)
+    # raw price
+    price = soup.find('td', class_='products_info_price').text
 
-        # raw price
-        price = soup.find('td', class_='products_info_price').text
+    # cleaned price
+    price = clean_price(price)
 
-        # cleaned price
-        price = clean_price(price)
+    # each product attribute is put into a list
+    data = [product_name, Category, image1, image2, description, price]
 
-        # each product attribute is put into a list
-        data = [product_name, Category, image1, image2, description, price]
-
-        return data
+    return data
 
 def get_data(pages): # iterate over pages and create dataframe
 
