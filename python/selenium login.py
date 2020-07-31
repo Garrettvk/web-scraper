afc656d9ca1672c6c88a44a91da17a0e0c58098d
Garrett <gvk795@gmail.com>
2020-07-31 01:28:10 -0700

    browser opens but wona wants cookies

--- python/selenium login.py
+++ python/selenium login.py
@@ -1,12 +1,24 @@
 from selenium import webdriver
 from getpass import getpass
 
-username_textbox = input("Enter your username: ")
-password = getpass("Enter your password: ") # hides password being typed
+# this is more secure than hard coding the credentials in the file
+# username = input("Enter your username: ")
+# password = getpass("Enter your password: ")  # hides password being typed
+
+# turn this off in production, use method up top ^
+username = 'tmebatson@gmail.com'
+password = 'hRIcV'
 
 # path to chromedriver
-driver = webdriver.chrome(r"C:\Users\admin\Anaconda3\Lib\site-packages\chromedriver\chromedriver.exe") 
+driver = webdriver.Chrome(r"C:\Users\admin\Anaconda3\Lib\site-packages\chromedriver\chromedriver.exe") 
 
 driver.get(r"https://www.wonatrading.com/login") # wona login page
 
-username_textbox = driver.find_element_by_id()
+username_textbox = driver.find_element_by_name('email_address') #find element for email input
+username_textbox.send_keys(username) # send what you put as userinput
+
+password_textbox = driver.find_element_by_name('password') #find element for password input
+password_textbox.send_keys(username) # send what you put as password
+
+login_button = driver.find_element_by_id('btnLogin') # element of login button
+login_button.submit() # simulates hitting enter key
