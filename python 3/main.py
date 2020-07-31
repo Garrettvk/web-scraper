71068937dec59235a6630d75c9cd529f0b128c38
Garrett <gvk795@gmail.com>
2020-07-31 10:03:20 -0700

    get links up to 125

--- python 3/main.py
+++ python 3/main.py
@@ -5,6 +5,6 @@ pd.set_option('display.max_colwidth', None) # prints entire column width
 pd.set_option('display.max_columns', None) # show all columns and rows
 pd.set_option('display.max_rows', None)
 
-pages = get_product_links()[0:2] # first 2 pages
+pages = get_product_links()[0:125] # first 2 pages
 df = get_data(pages) # dataframe from get_data function
 df
