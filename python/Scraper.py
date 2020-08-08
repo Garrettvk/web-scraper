1340d32e6f6f83b7e88644eb924cac5227ea41d8
Garrett <gvk795@gmail.com>
2020-08-08 04:44:15 -0700

    try except added + visted function added

--- python/Scraper.py
+++ python/Scraper.py
@@ -11,7 +11,11 @@ def get_driver(login_on = False): # return driver with no images
     options.add_experimental_option("prefs", prefs) # add option
     driver = webdriver.Chrome(options=options) # add option o webdriver
     # driver = login(driver) if login_on else driver # login diver
-    return driver # return driver
+    return driver  # return driver
+    
+def get_visted():
+    current_data = pd.read_csv('csv/webscraper_output.csv').fillna('') # open current data csv
+    return current_data['URL'].tolist() # return list of visted urls
 
 driver = get_driver()
 
@@ -25,17 +29,16 @@ class Scraper:
     def __init__(self, url):
         self.url = url
 
-        driver.get(self.url)  # first product page
+        driver.get(self.url)  # first product page    
 
-
-
-
-        soup = BeautifulSoup(driver.page_source, 'html5lib')  # soup = parsed html
-
-    
+        soup = BeautifulSoup(driver.page_source, 'html5lib')  # soup = parsed html    
 
         # data from html elements
-        self.name = soup.find('h2').text
+        try:
+            self.name = soup.find('h2').text
+        except AttributeError:
+            return
+
         self.categories = soup.find_all('a', style='font-family:eurof;font-size:14px;')
         self.categories = self.get_catagories()
         self.image_1 = self.domain + soup.find('img', id='main_img')['src']
@@ -64,7 +67,7 @@ class Scraper:
             ' / pc',
             '\$'
         ]
-        self.price = self.clean_data(price_patterns, self.price)
+        self.price = self.clean_data(price_patterns, self.price)    
 
     def clean_data(self, patterns = [], target = None):
         for pattern in patterns: # remove patterns and ignorecase
@@ -83,32 +86,43 @@ class Scraper:
         return matches_list[0].strip() # return 6 digit style number
 
 links = ['https://www.wonatrading.com/product_info.php?products_id=482328&kind=2&cPath=172_93_96&description=White-Gold-Dipped-Metal-Bar-Station-Anklet',
-'https://www.wonatrading.com/product_info.php?products_id=482465&kind=2&cPath=172_98&description=Two-Tone-Metal-Starfish-Pendant-Set']
+'https://www.wonatrading.com/product_info.php?products_id=482465&kind=2&cPath=172_98&description=Two-Tone-Metal-Starfish-Pendant-Set',
+'https://www.wonatrading.com/product_info.php?products_id=479524&kind=2&cPath=172_93_96&description=4PCS---Baby-Rhinestone-Chain-Layered-Anklets',
+'https://www.wonatrading.com/product_info.php?products_id=600520&kind=2&cPath=172_93_96&description=4PCS---Baby-Rhinestone-Chain-Layered-Anklets' ]
 
 
 df_list = []
 
 for link in links:
-    scrape_1 = Scraper(link)  # scraper object which takes url to be scraped
-    
-    columns = ['Product Name', 'Category', 'Product Image File - 1', 'Product Image File - 2', 'Product Description', 'Cost Price', 'URL', 'Style #']
-    data = [scrape_1.name, scrape_1.categories, scrape_1.image_1, scrape_1.image_2, scrape_1.description, scrape_1.price, scrape_1.url, scrape_1.style]
 
-    df = pd.DataFrame(columns=columns, data=[data])
+    if link in get_visted():
+        print('skip')
+    else:
 
-    df_list.append(df)
+        scrape_1 = Scraper(link)  # scraper object which takes url to be scraped
+        
+        columns = ['Product Name', 'Category', 'Product Image File - 1', 'Product Image File - 2', 'Product Description', 'Cost Price', 'URL', 'Style #']
+        
+        try:
+            data = [scrape_1.name, scrape_1.categories, scrape_1.image_1, scrape_1.image_2, scrape_1.description, scrape_1.price, scrape_1.url, scrape_1.style]
+        except AttributeError:
+            break
+
+        df = pd.DataFrame(columns=columns, data=[data])
+
+        df_list.append(df)
 
 
-    # print(scrape_1.name)
-    # print(scrape_1.categories)
-    # print(scrape_1.image_1)
-    # print(scrape_1.image_2)
-    # print(scrape_1.description)
-    # print(scrape_1.price)
-    # print(scrape_1.url)
-    # print(scrape_1.style)
+        # print(scrape_1.name)
+        # print(scrape_1.categories)
+        # print(scrape_1.image_1)
+        # print(scrape_1.image_2)
+        # print(scrape_1.description)
+        # print(scrape_1.price)
+        # print(scrape_1.url)
+        # print(scrape_1.style)
 
 output_df = pd.concat(df_list)
 
-output_df.to_csv('csv/webscraper_output.csv', index=False) # mode='a',
+output_df.to_csv('csv/webscraper_output.csv', mode='a', index=False, header = False) # 
 
