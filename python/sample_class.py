9992a918dfa0b163758730f6e40419ac9e9c1e6c
Garrett <gvk795@gmail.com>
2020-08-07 23:18:09 -0700

    product urls printed

--- python/sample_class.py
+++ python/sample_class.py
@@ -17,6 +17,9 @@ class Page:
         self.url = url
         self.category = self.get_category()
         self.count = self.get_product_count()
+        self.page_count = self.get_product_pages()
+        self.page_urls = self.get_page_urls()
+        self.product_urls = self.get_product_urls()
 
     def get_category(self):
         start = self.url.rindex('/') + 1
@@ -27,20 +30,42 @@ class Page:
         # text of element containing # of products converted to an int
         return int(driver.find_element_by_xpath('/html/body/div[1]/div/table[2]/tbody/tr/td[3]/table/tbody/tr/td[1]/table/tbody/tr[4]/td/form/table[2]/tbody/tr[4]/td/center/b[3]').text)
 
-    # def category_urls(self):
-    #     driver.get(self.url)  # open given url
-    #     # soup = parsed html
-    #     soup = BeautifulSoup(driver.page_source, 'html5lib')
-    #     # html element holding page numbers
-    #     html_elements = soup.find_all(self.tag, class_=self.class_)
-    #     # list of links for each category
-    #     return [page['href'] for page in html_elements]
+    def get_product_pages(self):
+        products_per_page = 100  # the number of products displayed on each page
 
-anklet_page = Page(f'{Page.domain}jewelry/anklet')
+        # divmod returns the # of whole pages and the remainder
+        number_of_pages, remainder = divmod(
+            self.count, products_per_page)
 
+        if number_of_pages == 0:  # if there are 0 pages
+            number_of_pages = 1  # 1 is the lowest amount of pages possible
+        else:  # if page > 0
+            if remainder > 0:  # if remainder exsists
+                number_of_pages += 1  # add a page
 
-# jewelry_page = Page('https://www.wonatrading.com/jewelry',
-#                     'category brrem', 'a')
+        return number_of_pages        
+
+    def get_page_urls(self):
+        return [self.url + f'/page={i}' for i in range(2, self.page_count + 1)]
+
+    def get_product_urls(self):
+        product_links = []  # this is product urls for all the products on the first page'
+
+        driver.get(self.url)  # first product page
+
+        soup = BeautifulSoup(driver.page_source, 'html5lib')  # parsed html
+
+        # this html element contains the href for the product
+        products = soup.find_all('a', style="position:relative;float:left;")
+
+        for product in products:  # iterate each product on webpage
+            product_link = product['href']  # assign href to a variable
+            product_link = product_link.replace('\r\n', '')  # remove hidden characters from href
+            domain = r'https://www.wonatrading.com/' # variable for domain
+            product_links.append(f'{domain}{product_link}')  # add domain
+                
+        return product_links
+
 
 category_pages = [
     'https://www.wonatrading.com/jewelry/anklet', 
@@ -56,37 +81,13 @@ category_pages = [
     'https://www.wonatrading.com/jewelry/stainless-steel', 
     'https://www.wonatrading.com/jewelry/watch', 
     'https://www.wonatrading.com/jewelry/jewelry-component', 
-    'https://www.wonatrading.com/jewelry/jewelry-display']
+    'https://www.wonatrading.com/jewelry/jewelry-display'
+    ]
+
 
 for page in category_pages:
     output = Page(page)
-    print(output.count)
-
-
-# class Product:
-
-#     def __init__(self, xpath):
-#         self.xpath = xpath
-
-#     def get_product_count(self):
-#         product_count_list = []  # this is a list of number of products for each category
-
-#         for category in Page.category_urls():
-
-#             driver.get(category)  # open page
-
-#             # text of element containing # of products converted to an int
-#             number_of_products = int(
-#                 driver.find_element_by_xpath(self.xpath).text)
-
-#             # add number of products to list
-#             product_count_list.append(number_of_products)
-
-#             return product_count_list
-
-
-# product_1 = Product(
-#     '/html/body/div[1]/div/table[2]/tbody/tr/td[3]/table/tbody/tr/td[1]/table/tbody/tr[4]/td/form/table[2]/tbody/tr[4]/td/center/b[3]')
+    print(output.product_urls)
 
 
 # exec(open('./python/sample_class.py').read()) # open python file in shell
